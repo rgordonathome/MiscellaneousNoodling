5312cf2b63bf571efd8bcdeceb8f540d1a74396c
Russell Gordon <rgordon@rsgc.on.ca>
2017-04-29 10:56:43 -0400

    Initial Commit

---
+++ MiscellaneousNoodling/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  MiscellaneousNoodling
+//
+//  Created by Russell Gordon on 4/29/17.
+//  Copyright © 2017 Russell Gordon. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
